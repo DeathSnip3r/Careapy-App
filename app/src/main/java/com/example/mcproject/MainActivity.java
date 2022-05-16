6bfb454804e90b1a39a8a5345f066e55aa14b586
AidanBrickH <aidanseventeen@gmail.com>
2022-05-16 21:43:39 +0200

    Added LoginActivity

--- app/src/main/java/com/example/mcproject/MainActivity.java
+++ app/src/main/java/com/example/mcproject/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mcproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.mcproject.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void btnclick(View v){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+
     }
 }
