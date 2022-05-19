e73533a98c4540356fd5d4ad6d2110b4f3a17732
AidanBrickH <aidanseventeen@gmail.com>
2022-05-19 19:56:45 +0200

    MOST RECENT

--- app/src/main/java/com/example/mcproject/F1Client.java
+++ app/src/main/java/com/example/mcproject/F1Client.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class F1Client extends Fragment {
 
@@ -19,7 +20,18 @@ public class F1Client extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_f1_client, container, false);
+
+
+        View rootView = inflater.inflate(R.layout.fragment_f1_client, container, false);
+        Spinner  spinnerLanguages= rootView.findViewById(R.id.spinnerLanguage);
+
+       // ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(, R.array.languages, android.R.layout.simple_spinner_item);
+       // adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+
         return inflater.inflate(R.layout.fragment_f1_client, container, false);
+
 
     }
 }
