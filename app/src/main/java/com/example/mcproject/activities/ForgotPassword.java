97600b6ac3eb36295be6dae57803777061077591
Mikyle Singh <mikylesingh159@gmail.com>
2022-06-04 20:20:34 +0200

    Commit -Small changes -Finished ForgotPassword and change_Password layout activities -Added next_ChangePass and changePass functions to java files

--- app/src/main/java/com/example/mcproject/activities/ForgotPassword.java
+++ app/src/main/java/com/example/mcproject/activities/ForgotPassword.java
@@ -3,6 +3,7 @@ package com.example.mcproject.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.mcproject.R;
 
@@ -12,5 +13,9 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+    }
+
+    public void next_ChangePass(View v){
+
     }
 }
--- app/src/main/java/com/example/mcproject/activities/change_Password.java
+++ app/src/main/java/com/example/mcproject/activities/change_Password.java
@@ -3,6 +3,7 @@ package com.example.mcproject.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.mcproject.R;
 
@@ -12,5 +13,9 @@ public class change_Password extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+    }
+
+    public void ChangePass(View v){
+
     }
 }
