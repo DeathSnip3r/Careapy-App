31b75716ce418b314157344115267307386fd0ce
Mikyle Singh <mikylesingh159@gmail.com>
2022-06-04 14:49:33 +0200

    Commit -Small changes -Added ForgotPassword and change_Password activities

---
+++ app/src/main/java/com/example/mcproject/activities/change_Password.java
@@ -0,0 +1,16 @@
+package com.example.mcproject.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mcproject.R;
+
+public class change_Password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
